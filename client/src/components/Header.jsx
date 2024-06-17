2f395be6a43252fa4a8102ce8d32165aa4a97354
m-jonathan-g <7mg.jonathan@gmail.com>
2024-06-16 16:45:18 -0900

    .

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ import { UserContext } from './UserContext';
 export default function Header(){
     const {setUserInfo, userInfo} = useContext(UserContext)
     useEffect(() => {
-        fetch('https://localhost:4001/profile', {
+        fetch('https://localhost:3000/profile', {
             credentials: 'include'
         }).then(response => {
             response.json().then(userInfo => {
@@ -17,7 +17,7 @@ export default function Header(){
     }, [])
 
     function logout() {
-        fetch('http://localhost:4001/logout', {
+        fetch('http://localhost:3000/logout', {
             credentials: 'include',
             method: 'POST' ,
         })
--- client/src/pages/BlogPost.jsx
+++ client/src/pages/BlogPost.jsx
@@ -9,7 +9,7 @@ export default function PostPage() {
   const {userInfo} = useContext(UserContext);
   const {id} = useParams();
   useEffect(() => {
-    fetch(`http://localhost:4001/post/${id}`)
+    fetch(`http://localhost:3000/post/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setPostInfo(postInfo);
@@ -35,7 +35,7 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`http://localhost:4001/${postInfo.cover}`} alt=""/>
+        <img src={`http://localhost:3000/${postInfo.cover}`} alt=""/>
       </div>
       <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
     </div>
